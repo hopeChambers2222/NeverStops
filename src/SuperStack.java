373f5699663826d07316a5135cf2e2fc575f611e
hopeChambers2222 <60853188+hopeChambers2222@users.noreply.github.com>
2020-10-21 16:38:42 -0500

    Add files via upload

---
+++ src/SuperStack.java
@@ -0,0 +1,24 @@
+
+public class SuperStack<T> {
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T>(int size){
+		items = (T[]) new Object[];
+	}
+	public boolean isEmpty() {
+		
+	}
+	
+	public boolean isFull() {
+		
+	}
+	
+	public boolean push(T item) {
+		
+	}
+	
+	public T pop() {
+		
+	}
+}
